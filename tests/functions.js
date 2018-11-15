4e21eb3ec7ba4c0fab759787660e57c57e672cd7
agj <ale@agj.cl>
2018-11-15 20:27:11 -0300

    Testing library functions.

--- tests/functions.js
+++ tests/functions.js
@@ -1,23 +1,45 @@
 
-const dup = a => [a, a];
-const swap = (a, b) => [b, a];
-const drop = a => [];
+const test = require('blue-tape');
 
-const add = (a, b) => [a + b];
-const subtract = (a, b) => [a - b];
-const multiply = (a, b) => [a * b];
-const divide = (a, b) => [a / b];
-
-const uppercase = a => [a.toUpperCase()];
+const conexp = require('../sync');
+const functions = require('../functions');
 
 
-module.exports = {
-	dup,
-	swap,
-	drop,
-	add,
-	subtract,
-	multiply,
-	divide,
-	uppercase,
-};
+test("Functions: Number matches.", assert => {
+	assert.plan(1);
+	assert.equals(Object.keys(functions).length, 4);
+});
+
+test("Functions: id.", assert => {
+	assert.plan(1);
+	const evaluate = conexp({ id: functions.id });
+	assert.deepEquals(
+		evaluate(`true id 5 id`),
+		[true, 5]);
+});
+
+test("Functions: dup.", assert => {
+	assert.plan(1);
+	const evaluate = conexp({ dup: functions.dup });
+	assert.deepEquals(
+		evaluate(`true dup 5 dup`),
+		[true, true, 5, 5]);
+});
+
+test("Functions: drop.", assert => {
+	assert.plan(1);
+	const evaluate = conexp({ drop: functions.drop });
+	assert.deepEquals(
+		evaluate(`1 2 drop 3`),
+		[1, 3]);
+});
+
+test("Functions: swap.", assert => {
+	assert.plan(1);
+	const evaluate = conexp({ swap: functions.swap });
+	assert.deepEquals(
+		evaluate(`1 2 swap`),
+		[2, 1]);
+});
+
+
--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,6 @@ const test = require('blue-tape');
 const conexp = require('../sync');
 const functions = require('../functions');
 const { func, metaFunc } = conexp;
-const f = require('./functions');
 
 
 const add = func((a, b) => [a + b]);
@@ -16,31 +15,5 @@ const uppercase = func(a => [a.toUpperCase()]);
 
 
 require('./parsing');
-
-
-test("Sync: Move integers around with functions.", assert => {
-	assert.plan(1);
-	const evaluate = conexp({ dup: functions.dup, swap: functions.swap, drop: functions.drop });
-	assert.deepEquals(
-		evaluate('1 2 3 swap drop dup'),
-		[1, 3, 3]);
-});
-
-test("Sync: Operate on integers.", assert => {
-	assert.plan(1);
-	const evaluate = conexp({ add: add, subtract: subtract, multiply: multiply, divide: divide, swap: functions.swap });
-	assert.deepEquals(
-		evaluate('32 4 3 2 1 subtract add multiply divide'),
-		[2]);
-});
-
-test("Sync: Operate on strings.", assert => {
-	assert.plan(1);
-	const evaluate = conexp({ uppercase: uppercase });
-	assert.deepEquals(
-		evaluate('"Some" "Random String" uppercase'),
-		['Some', 'RANDOM STRING']);
-});
-
-
+require('./functions');
 
